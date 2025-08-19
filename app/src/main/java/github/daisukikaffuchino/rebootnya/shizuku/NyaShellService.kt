c6bf3d7d04c8a05a96f7e5da29e8a49f88c8a5f3
KaffuChino <47238156+daisukiKaffuChino@users.noreply.github.com>
2025-08-19 16:02:08 +0800

    feat: detect shell timeout or not

--- app/src/main/java/github/daisukikaffuchino/rebootnya/shizuku/NyaShellService.kt
+++ app/src/main/java/github/daisukikaffuchino/rebootnya/shizuku/NyaShellService.kt
@@ -5,6 +5,10 @@ import android.os.RemoteException
 import android.util.Log
 import androidx.annotation.Keep
 import github.daisukikaffuchino.rebootnya.IShellService
+import java.util.concurrent.Callable
+import java.util.concurrent.Executors
+import java.util.concurrent.TimeUnit
+import java.util.concurrent.TimeoutException
 
 
 class NyaShellService : IShellService.Stub {
@@ -19,12 +23,28 @@ class NyaShellService : IShellService.Stub {
 
     @Throws(RemoteException::class)
     override fun exec(cmd: String): Int {
-        try {
-            val process = Runtime.getRuntime().exec(cmd)
-            return process.waitFor()
+        //var process: Process? = null
+        return executeCommandWithTimeout(cmd, 3000)
+    }
+
+    fun executeCommandWithTimeout(cmd: String, timeoutMs: Long): Int {
+        val executor = Executors.newSingleThreadExecutor()
+        val future = executor.submit(Callable {
+            Runtime.getRuntime().exec(cmd).waitFor()
+        })
+
+        return try {
+            future.get(timeoutMs, TimeUnit.MILLISECONDS)
+        } catch (e: TimeoutException) {
+            Log.d("ShellService", "Timeout:\n" + e.message.toString())
+            future.cancel(true)
+            -2 //超时
         } catch (e: Exception) {
-            e.fillInStackTrace()
-            return -1
+            Log.d("ShellService", "Error:\n" + e.message.toString())
+            -1
+        } finally {
+            executor.shutdown()
         }
     }
+
 }
