9bff76fe113f05a418795c0a0b068d059329644b
KaffuChino <47238156+daisukiKaffuChino@users.noreply.github.com>
2025-08-19 23:33:30 +0800

    ci: bump version to 1.3.2

--- app/src/main/java/github/daisukikaffuchino/rebootnya/MainActivity.kt
+++ app/src/main/java/github/daisukikaffuchino/rebootnya/MainActivity.kt
@@ -2,14 +2,10 @@ package github.daisukikaffuchino.rebootnya
 
 import android.graphics.Color
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.graphics.drawable.toDrawable
 import com.google.android.material.color.DynamicColors
 import github.daisukikaffuchino.rebootnya.databinding.ActivityMainBinding
-import github.daisukikaffuchino.rebootnya.shizuku.NyaShellManager
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,13 +19,5 @@ class MainActivity : AppCompatActivity() {
 
         val binding: ActivityMainBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.getRoot())
-
-        val handler = Handler(Looper.getMainLooper())
-        handler.postDelayed({
-            NyaShellManager.bindService { exitCode, message ->
-
-            }
-        }, 5000)
-
     }
 }
